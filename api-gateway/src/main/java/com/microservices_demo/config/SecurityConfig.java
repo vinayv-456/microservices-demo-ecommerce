d76fccaa7e6e86fb514edf2da6ecef55f5247a72
vinayv-456 <vinayv.456@gmail.com>
2025-10-08 19:05:56 -0400

    feat: add keyclock of outh authentication and integrated to api-gateway

--- api-gateway/src/main/java/com/microservices_demo/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/microservices_demo/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.microservices_demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -16,8 +17,9 @@ public class SecurityConfig {
                 .authorizeExchange(exchange -> exchange.pathMatchers("/eureka/**")
                         .permitAll()
                         .anyExchange()
-                        .authenticated());
+                        .authenticated())
+                .oauth2ResourceServer(
+                        oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec.jwt(Customizer.withDefaults()));
         return serverHttpSecurity.build();
-
     }
 }
